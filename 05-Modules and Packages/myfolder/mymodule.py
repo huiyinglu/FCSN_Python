9ddf4e7ca4e6a17003a37adf549b58a050ec1f1f
HUI-YING (Kristin) Lu <huiying_lu@yahoo.com>
2023-01-03 02:15:54 -0800

    Add files via upload

---
+++ 05-Modules and Packages/myfolder/mymodule.py
@@ -0,0 +1,6 @@
+def even_check(my_iter):
+    for num in my_iter:
+        if num % 2 != 0:
+            return False
+    return True
+
---
+++ 05-Modules and Packages/myprog.py
@@ -0,0 +1,4 @@
+def increment(a, incr=1):
+    a = a + incr
+    return a
+
